a55c8a3d6ff5f896e1aceb86e404f644cb0b6a1c
Mahmoud Magdi <mahmoudmagdi@ymail.com>
2023-05-21 02:37:08 +0300

    SDK now is compatible with iOS 13.0

--- Sources/QattahWebView.swift
+++ Sources/QattahWebView.swift
@@ -21,18 +21,36 @@ public struct QattahWebView: View {
     public var body: some View {
         NavigationView {
             VStack {
-                CustomWebView(qattahResponse: self.qattahResponse)
+                CustomWebView(qattahResponse: self.qattahResponse, qattahPaymentCallback: self.qattahPaymentCallback!)
             }
-        }.onAppear {
-            startSocketListener()
         }
     }
     
-    private func startSocketListener() {
-        self.qattahPaymentCallback?.onStarted(paymentId: (qattahResponse.data?.id)!)
-        let manager = SocketManager(socketURL: URL(string: "https://testing-callback.qattahpay.sa")!, config: [.log(false), .compress])
-        //let socket = manager.defaultSocket
+}
+
+@available(iOS 13.0, *)
+public struct CustomWebView: UIViewRepresentable {
+
+    public typealias UIViewType = WKWebView
+    let webView: WKWebView
+
+    public init(qattahResponse: QattahResponse?, qattahPaymentCallback: PaymentCallback) {
+
+        let requiredUrl = qattahResponse?.links?.redirect_to
+        webView = WKWebView(frame: .zero)
         
+        if (requiredUrl != nil) {
+            webView.load(URLRequest(url: URL(string: requiredUrl!)!))
+            startSocketListener(qattahResponse: qattahResponse!, qattahPaymentCallback: qattahPaymentCallback)
+        } else {
+            //TODO: navigate back
+        }
+    }
+
+    private func startSocketListener(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback) {
+        
+        qattahPaymentCallback.onStarted(paymentId: (qattahResponse.data?.id)!)
+        let manager = SocketManager(socketURL: URL(string: "https://testing-callback.qattahpay.sa")!, config: [.log(false), .compress])
         let socket = SocketIOClient(manager: manager, nsp: "/")
 
         socket.on(clientEvent: .connect) { data, ack in
@@ -46,54 +64,34 @@ public struct QattahWebView: View {
             let arr = data as? [[String: Any]]
             let paymentStatus = arr![0]["paymentStatus"] as? String
             print(paymentStatus!)
-            onNewMessage(newMessage: paymentStatus!)
+            onNewMessage(newMessage: paymentStatus!, qattahResponse: qattahResponse, qattahPaymentCallback: qattahPaymentCallback)
         }
         
         socket.on(clientEvent: .disconnect) { data, ack in
             print("DISCONNECTED" + ((data[0] as AnyObject) as! String))
-            self.qattahPaymentCallback?.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
+            qattahPaymentCallback.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
         }
         
         socket.on(clientEvent: .error) { data, ack in
             print("CONECTION_ERROR" + ((data[0] as AnyObject) as! String))
-            self.qattahPaymentCallback?.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
+            qattahPaymentCallback.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
         }
         socket.connect()
     }
     
-    private func onNewMessage(newMessage: String) {
+    private func onNewMessage(newMessage: String, qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback) {
         switch newMessage {
             
         case "PAID":
-                self.qattahPaymentCallback?.onSuccess(paymentId: (qattahResponse.data?.id)!)
+                qattahPaymentCallback.onSuccess(paymentId: (qattahResponse.data?.id)!)
            
         case "REFUNDED":
-                self.qattahPaymentCallback?.onError(errorMessage: "Qattah Pay order is expired")
-            
+                qattahPaymentCallback.onError(errorMessage: "Qattah Pay order is expired")
         default:
                 return
         }
     }
-}
-
-@available(iOS 13.0, *)
-public struct CustomWebView: UIViewRepresentable {
-
-    public typealias UIViewType = WKWebView
-    let webView: WKWebView
-
-    public init(qattahResponse: QattahResponse?) {
-
-        let requiredUrl = qattahResponse?.links?.redirect_to
-        webView = WKWebView(frame: .zero)
-        
-        if (requiredUrl != nil) {
-            webView.load(URLRequest(url: URL(string: requiredUrl!)!))
-        } else {
-            //TODO: navigate back
-        }
-    }
-
+    
     public func makeUIView(context: Context) -> WKWebView {
         webView
     }
