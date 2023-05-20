7abf26622364299f0e2a37f96ddaa80ff44891d0
Mahmoud Magdi <mahmoudmagdi@ymail.com>
2023-05-21 00:21:41 +0300

    SDK now is compatible with iOS 13.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "qattahpay-ios-sdk", targets: ["qattahpay-ios-sdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "15.0.0"))
+        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.0.1"))
     ],
     targets: [
         .target(name: "qattahpay-ios-sdk", dependencies: ["SocketIO"], path: "Sources")
