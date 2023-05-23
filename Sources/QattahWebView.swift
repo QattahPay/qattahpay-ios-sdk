e62859f80a7b74b9d9a77ba9befcf8d8c857f1dc
Mahmoud Magdi <mahmoudmagdi@ymail.com>
2023-05-23 13:58:55 +0300

    handle back action

--- Sources/QattahWebView.swift
+++ Sources/QattahWebView.swift
@@ -15,6 +15,10 @@ public struct QattahWebView: View {
     
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
+    let manager = SocketManager(socketURL: URL(string: "https://testing-callback.qattahpay.sa")!, config: [.log(false), .compress])
+    
+    @State var socket: SocketIOClient? = nil
+
     public init(qattahResponse: QattahResponse?, qattahPaymentCallback: PaymentCallback) {
         self.qattahResponse = qattahResponse ?? QattahResponse()
         self.qattahPaymentCallback = qattahPaymentCallback
@@ -23,82 +27,82 @@ public struct QattahWebView: View {
     public var body: some View {
         NavigationView {
             VStack {
-                CustomWebView(qattahResponse: self.qattahResponse, qattahPaymentCallback: self.qattahPaymentCallback!)
+                CustomWebView(qattahResponse: self.qattahResponse, qattahPaymentCallback: self.qattahPaymentCallback!) {
+                    self.startSocketListener()
+                }
             }
         }.navigationBarBackButtonHidden(true)
             .navigationBarItems(leading: Button(action : {
                 self.mode.wrappedValue.dismiss()
                 self.qattahPaymentCallback?.onCancel()
+                self.socket?.disconnect()
             }) {
                 Image(systemName: "arrow.left")
             })
     }
     
+    private func startSocketListener() {
+        
+        self.socket = manager.defaultSocket
+        
+        socket?.on(clientEvent: .connect) { data, ack in
+            self.qattahPaymentCallback?.onStarted(paymentId: (qattahResponse.data?.order.id)!)
+            print("CONNECTED" + ((data[0] as AnyObject) as! String))
+            socket?.emit("join-room", (qattahResponse.data?.order.id)!)
+        }
+        
+        socket?.on("update-payment") { data, ack in
+            print("update-payment")
+            
+            let arr = data as? [[String: Any]]
+            let paymentStatus = arr![0]["paymentStatus"] as? String
+            print(paymentStatus!)
+            self.onNewMessage(newMessage: paymentStatus!, qattahResponse: qattahResponse, qattahPaymentCallback: self.qattahPaymentCallback)
+        }
+        
+        socket?.on(clientEvent: .disconnect) { data, ack in
+            print("DISCONNECTED" + ((data[0] as AnyObject) as! String))
+            self.qattahPaymentCallback?.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
+        }
+        
+        socket?.on(clientEvent: .error) { data, ack in
+            print("CONECTION_ERROR" + ((data[0] as AnyObject) as! String))
+            self.qattahPaymentCallback?.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
+        }
+        
+        socket?.connect()
+    }
+    
+    private func onNewMessage(newMessage: String, qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback?) {
+        switch newMessage {
+            
+        case "PAID":
+            qattahPaymentCallback?.onSuccess(paymentId: (qattahResponse.data?.order.id)!)
+           
+        case "REFUNDED":
+                qattahPaymentCallback?.onError(errorMessage: "Qattah Pay order is expired")
+        default:
+                return
+        }
+    }
 }
 
 @available(iOS 13.0, *)
 public struct CustomWebView: UIViewRepresentable {
-
-    let manager = SocketManager(socketURL: URL(string: "https://testing-callback.qattahpay.sa")!, config: [.log(false), .compress])
     
     public typealias UIViewType = WKWebView
     let webView: WKWebView
-
-    public init(qattahResponse: QattahResponse?, qattahPaymentCallback: PaymentCallback) {
+    
+    public init(qattahResponse: QattahResponse?, qattahPaymentCallback: PaymentCallback, onStart: @escaping () -> Void) {
 
         let requiredUrl = qattahResponse?.links?.redirect_to
         webView = WKWebView(frame: .zero)
         
         if (requiredUrl != nil) {
             webView.load(URLRequest(url: URL(string: requiredUrl!)!))
-            startSocketListener(qattahResponse: qattahResponse!, qattahPaymentCallback: qattahPaymentCallback)
+            onStart()
         } else {
             //TODO: navigate back
-        }
-    }
-
-    private func startSocketListener(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback) {
-        
-        let socket = manager.defaultSocket
-        
-        socket.on(clientEvent: .connect) { data, ack in
-            qattahPaymentCallback.onStarted(paymentId: (qattahResponse.data?.order.id)!)
-            print("CONNECTED" + ((data[0] as AnyObject) as! String))
-            socket.emit("join-room", (qattahResponse.data?.order.id)!)
-        }
-        
-        socket.on("update-payment") { data, ack in
-            print("update-payment")
-            
-            let arr = data as? [[String: Any]]
-            let paymentStatus = arr![0]["paymentStatus"] as? String
-            print(paymentStatus!)
-            onNewMessage(newMessage: paymentStatus!, qattahResponse: qattahResponse, qattahPaymentCallback: qattahPaymentCallback)
-        }
-        
-        socket.on(clientEvent: .disconnect) { data, ack in
-            print("DISCONNECTED" + ((data[0] as AnyObject) as! String))
-            qattahPaymentCallback.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
-        }
-        
-        socket.on(clientEvent: .error) { data, ack in
-            print("CONECTION_ERROR" + ((data[0] as AnyObject) as! String))
-            qattahPaymentCallback.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
-        }
-        
-        socket.connect()
-    }
-    
-    private func onNewMessage(newMessage: String, qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback) {
-        switch newMessage {
-            
-        case "PAID":
-            qattahPaymentCallback.onSuccess(paymentId: (qattahResponse.data?.order.id)!)
-           
-        case "REFUNDED":
-                qattahPaymentCallback.onError(errorMessage: "Qattah Pay order is expired")
-        default:
-                return
         }
     }
     
