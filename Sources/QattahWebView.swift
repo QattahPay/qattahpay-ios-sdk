4fd7469f3a71f1e7e2d2d1e53c31fa7efe7776e5
Mahmoud Magdi <mahmoudmagdi@ymail.com>
2023-07-18 03:36:38 +0300

    new expiration timer added

--- Sources/QattahWebView.swift
+++ Sources/QattahWebView.swift
@@ -49,7 +49,7 @@ public struct CustomWebView: UIViewRepresentable {
 
     let manager = SocketManager(socketURL: URL(string: "https://testing-callback.qattahpay.sa")!, config: [.log(false), .compress])
     
-    @State private var remainingMin = 1
+    @State private var remainingMin = 15
     @State private var remainingSec = 0
     
     public typealias UIViewType = WKWebView
