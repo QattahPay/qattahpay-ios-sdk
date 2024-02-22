55fe33b454450fb53b480f1b38201fea592c499b
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-02-22 03:48:13 +0200

    Realtime updates

--- Sources/QattahWebView.swift
+++ Sources/QattahWebView.swift
@@ -20,7 +20,7 @@ public struct QattahWebView: View {
         self.qattahResponse = qattahResponse ?? QattahResponse()
         self.qattahPaymentCallback = qattahPaymentCallback
         
-        self.customWebView = CustomWebView(qattahResponse: self.qattahResponse, qattahPaymentCallback: self.qattahPaymentCallback!, qattahWebView: self)
+        self.customWebView = CustomWebView(qattahResponse: self.qattahResponse, qattahPaymentCallback: self.qattahPaymentCallback, qattahWebView: self)
     }
     
     public var body: some View {
@@ -64,7 +64,7 @@ public struct CustomWebView: UIViewRepresentable {
     
     var socket: SocketIOClient? = nil
 
-    public init(qattahResponse: QattahResponse?, qattahPaymentCallback: PaymentCallback, qattahWebView: QattahWebView) {
+    public init(qattahResponse: QattahResponse?, qattahPaymentCallback: PaymentCallback?, qattahWebView: QattahWebView) {
 
         let requiredUrl = qattahResponse?.links?.redirect_to
         webView = WKWebView(frame: .zero)
@@ -78,7 +78,7 @@ public struct CustomWebView: UIViewRepresentable {
         }
     }
     
-    private mutating func startSocketListener(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback) {
+    private mutating func startSocketListener(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback?) {
         
         self.socket = manager.defaultSocket
         let selfItem = self
@@ -93,25 +93,25 @@ public struct CustomWebView: UIViewRepresentable {
         
         self.socket?.on(clientEvent: .disconnect) { data, ack in
             print("DISCONNECTED")
-            qattahPaymentCallback.onError(errorMessage: "Qattah Pay socket disconnected.")
+            qattahPaymentCallback?.onError(errorMessage: "Qattah Pay socket disconnected.")
         }
         
         self.socket?.on(clientEvent: .error) { data, ack in
             print("CONECTION_ERROR")
-            qattahPaymentCallback.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
+            qattahPaymentCallback?.onError(errorMessage: "Qattah Pay socket connection lost, please check internet connection.")
         }
         
         self.socket?.connect()
         
     }
     
-    private func connectionHandling(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback, data: [Any]) {
-        qattahPaymentCallback.onStarted(paymentId: (qattahResponse.data?.order.id)!)
+    private func connectionHandling(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback?, data: [Any]) {
+        qattahPaymentCallback?.onStarted(paymentId: (qattahResponse.data?.order.id)!)
         print("CONNECTED" + ((data[0] as AnyObject) as! String))
         socket?.emit("join-room", (qattahResponse.data?.order.id)!)
     }
     
-    private func updatePayment(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback, data: [Any]) {
+    private func updatePayment(qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback?, data: [Any]) {
         let arr = data as? [[String: Any]]
         let paymentStatus = arr![0]["paymentStatus"] as? String
         print(paymentStatus!)
@@ -142,17 +142,17 @@ public struct CustomWebView: UIViewRepresentable {
         })
     }
     
-    private func onNewMessage(newMessage: String?, qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback) {
+    private func onNewMessage(newMessage: String?, qattahResponse: QattahResponse, qattahPaymentCallback: PaymentCallback?) {
         switch newMessage {
             
         case "PAID":
-            qattahPaymentCallback.onSuccess(paymentId: (qattahResponse.data?.order.id)!)
+            qattahPaymentCallback?.onSuccess(paymentId: (qattahResponse.data?.order.id)!)
             
         case "AUTHORIZED":
-            qattahPaymentCallback.onSuccess(paymentId: (qattahResponse.data?.order.id)!)
+            qattahPaymentCallback?.onSuccess(paymentId: (qattahResponse.data?.order.id)!)
            
         case "REFUNDED":
-                qattahPaymentCallback.onError(errorMessage: "Qattah Pay order is expired")
+                qattahPaymentCallback?.onError(errorMessage: "Qattah Pay order is expired")
         default:
                 return
         }
